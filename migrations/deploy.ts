d4ca9629292878c3908269d317b5f73d253bd265
Ainul Ξ Creative Dev <hey@moojok.space>
2025-06-24 14:00:57 +0700

    Update wallet path in Anchor.toml, enhance deploy script logging, and add ts-node dependency in package.json

--- migrations/deploy.ts
+++ migrations/deploy.ts
@@ -1,3 +1,8 @@
+process.chdir(__dirname + "/..");
+console.log("Changed working directory to:", process.cwd());
+
+console.log("Current working directory:", process.cwd());
+
 // Migrations are an early feature. Currently, they're nothing more than this
 // single deploy script that's invoked from the CLI, injecting a provider
 // configured from the workspace's Anchor.toml.
